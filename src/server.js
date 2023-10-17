3a273bd86396f5d6ce6b9ec64786f4aedc526701
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-10-17 19:48:45 -0400

    Creating routes within the `server.js`

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/') {
+    res.end('Home da aplicacao')
+  }
+
+  if (method === 'GET' && url === '/tasks') {
+    return res.end('Listagem de tasks')
+  }
+
+  if (method === 'POST' && url === '/tasks') {
+    return res.end('Criação de tasks')
+  }
+  
 })
 
 const port = 3333
-
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
